0836cc3a6a79695dd7720d14f8ac53d55496aa07
BigLofanbl4 <smallofanbl4@gmail.com>
2024-04-29 16:50:41 +0300

    ex2

---
+++ individuals/ex2.py
@@ -0,0 +1,41 @@
+# #!/usr/bin/env python
+# # -*- coding: utf-8 -*-
+
+from random import randint
+
+
+if __name__ == "__main__":
+    rows = input("rows: ")
+    columns = input("columns: ")
+    min_val = input("min_val: ")
+    max_val = input("max_val: ")
+
+    try:
+        rows = int(rows)
+        columns = int(columns)
+        min_val = int(min_val)
+        max_val = int(max_val)
+
+        if rows <= 0 or columns <= 0:
+            raise ValueError("Rows & Colums must be > 0!")
+
+        if min_val >= max_val:
+            raise ValueError("min_val must be < max_val!")
+    except ValueError as e:
+        print(e)
+        exit(1)
+
+    # matrix = []
+    # for i in range(rows):
+    #     row = []
+    #     for j in range(columns):
+    #         num = randint(min_val, max_val)
+    #         row.append(num)
+    #     matrix.append(row)
+
+    matrix = [
+        [randint(min_val, max_val) for _ in range(columns)] for _ in range(rows)
+    ]
+    print("Generated matrix:")
+    for row in matrix:
+        print(row)
